1eee9f553ee7b89048be6c51252e5119bb1aaabd
code810 <nadirssh@code.edu.az>
2024-03-31 16:13:51 +0400

    Domain library and base class ready

---
+++ HrManagementApp.Domain/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using HrManagementApp.Domain.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrManagementApp.Domain.Models
+{
+    public class Employee: BaseEntity
+    {
+        public string No { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public int Salary { get; set; }
+        public Department DepartmentName { get; set; }
+    }
+}
