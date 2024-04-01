69240867a25bc39aa7396db78bde782cac8a7d8b
code810 <nadirssh@code.edu.az>
2024-04-01 12:22:52 +0400

    DepartmentRepository created

--- HrManagementApp.DataContext/Repository/DepartmentRepository.cs
+++ HrManagementApp.DataContext/Repository/DepartmentRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HrManagementApp.DataContext.Interface;
+using HrManagementApp.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,63 @@ using System.Threading.Tasks;
 
 namespace HrManagementApp.DataContext.Repository
 {
-    internal class DepartmentRepository
+    public class DepartmentRepository : IRepository<Department>
     {
+        public bool Creat(Department entity)
+        {
+            try
+            {
+                DbContext.Departments.Add(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Delete(Department entity)
+        {
+            try
+            {
+                DbContext.Departments.Remove(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Department Get(Predicate<Department> filter)
+        {
+           return DbContext.Departments.Find(filter);
+
+        }
+
+        public List<Department> GetAll(Predicate<Department> filter = null)
+        {
+            return filter is null? DbContext.Departments : DbContext.Departments.FindAll(filter);
+        }
+
+        public bool Update(Department entity)
+        {
+            try
+            {
+                var existDepartmentIndex = DbContext.Departments.FindIndex(u=>u.Id==entity.Id);
+                if (existDepartmentIndex != -1)
+                {
+                    DbContext.Departments[existDepartmentIndex] = entity;
+                return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
